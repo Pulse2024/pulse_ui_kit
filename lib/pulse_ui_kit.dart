b8f67a070639ce3cf20689b879759c1d4330423c
MartinDev1902 <martin.tefive1902@gmail.com>
2024-03-13 16:58:13 +0100

    PButton Component Created and Tests Removed

--- lib/pulse_ui_kit.dart
+++ lib/pulse_ui_kit.dart
@@ -1,7 +1,3 @@
 library pulse_ui_kit;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:pulse_ui_kit/components/Button.dart';
